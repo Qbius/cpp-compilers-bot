3a37d7a014825ec1c6d364725cf1e6f5029ab3cf
Qbius <mich@lsurowka.email>
2019-11-19 22:26:57 +0100

    commands now work

--- discord_bot_interface.py
+++ discord_bot_interface.py
@@ -7,46 +7,48 @@ def load_state():
 def save_state(obj):
     pickle.dump(obj.__dict__, open('state.info', 'wb'))
 
-def __check_if_last_arguments(fun, args):
+def check_if_last_arguments(fun, args):
     fun_params = list(signature(fun).parameters)
     kwarg_params = [k for k in fun_params if k in ['message', 'state']]
     return set(kwarg_params) - set(fun_params[-len(kwarg_params):])
     
 import discord
 import asyncio
-from inspect import signature
+from inspect import signature, Parameter
 
 client = discord.Client()
 print('Initializing...')
 
-available_commands = dict()
+
 class command(object):
 
+    available_commands = dict()
+
     @staticmethod
-    def __add_command(prefix, fun):
-        not_last_special_args = __check_if_last_arguments(fun, ['message, state'])
+    def add_command(prefix, fun):
+        not_last_special_args = check_if_last_arguments(fun, ['message, state'])
         if len(not_last_special_args) > 0:
             raise Error(f"Error while trying to add the {fun.__name__} command: {', '.join(not_last_special_args)} not at the back of the argument list")
         else:
-            available_commands[f'{prefix}{fun.__name__}'] = fun
+            command.available_commands[f'{prefix}{fun.__name__}'] = fun
             print(f"Added command {fun.__name__}")
 
     def __init__(self, fun):
-        __add_command('!', fun)
+        command.add_command('!', fun)
 
     @staticmethod
     def override_prefix(pref):
-        return lambda fun: __add_command(pref, fun)
+        return lambda fun: command.add_command(pref, fun)
 
     @staticmethod
-    def __call_command(msg):
+    def call_command(msg):
         cmd_name, *args = msg.content.split(' ')
-        if cmd_name not in available_commands: return
-        cmd = available_commands[cmd_name]
+        if cmd_name not in command.available_commands: return
+        cmd = command.available_commands[cmd_name]
         fun_params = signature(cmd).parameters
-        kwargs = {k: v for k, v in {'message': msg, 'state': load('state')}.items() if k in fun_params}
+        kwargs = {k: v for k, v in {'message': msg, 'state': load_state()}.items() if k in fun_params}
 
-        normal_args = [k for k in fun_params.keys() if k not in all_kwargs.keys()]
+        normal_args = [k for k in fun_params.keys() if k not in kwargs.keys()]
         if len(normal_args) != len(args):
             args_err_msg = f"Error, expected {len(normal_args)} arguments (got {len(args)})\nExpected arguments: "
             args_err_msg += ','.join([f'{arg_name}' + (f' (default: {fun_params.get(arg_name).default})' if fun_params.get(arg_name).default != Parameter.empty else '') for arg_name in normal_args])
@@ -56,10 +58,10 @@ class command(object):
 
 @client.event
 async def on_message(msg):
-    command.__call_command(msg)
+    command.call_command(msg)
 
 def event(fun):
-    if __check_if_last_arguments(fun, ['state']):
+    if check_if_last_arguments(fun, ['state']):
         raise Error("state needs to be the last argument")
 
     def inner(*a, **kw):
