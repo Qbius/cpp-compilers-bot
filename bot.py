dbd60f97939543cf179d027625966de9220b85b5
Qbius <mich@lsurowka.email>
2019-11-20 00:50:13 +0100

    further bot improvements

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
-from discord_bot_interface import event, command, task, run
+from cpp_reference import get_cpp_features_map
+from discord_bot_interface import command, task, run, create_state, client
 
 @command
-def subscribe(hehe, state):
-    state.hehe = hehe
+def subscribe(message, state):
+    state.subs.add(message.author.id)
 
 @command
-def get(state):
-    return state.hehe
+def unsubscribe(message, state):
+    state.subs.remove(message.author.id)
 
-run()
+@task.loop(seconds = 60)
+def check_cpp_reference(state):
+    old_features = state.features
+    new_features = get_cpp_features_map()
+    for version, new_features_set in new_features.items():
+        
+
+    {version: diff for version, new_feature_set in new_features.items() if (diff := (new_feature_set - old_features[version]))}
+    return [client.get_user(usr_id).send("hehe") for usr_id in state.subs]
+
+run(default_state = create_state(subs = set(), features = get_cpp_features_map()))
--- cpp_reference.py
+++ cpp_reference.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+from random import randint
+
 def features_table_to_set(table):
     features = [tr for tr in table.find_all("tr") if 'style' not in tr.attrs.keys()]
-    return {tuple(td.text.strip() for td in feature.find_all("td")[:5]) for feature in features} # the 5 elements are as follows: Name, Proposal paper id, GCC Version, Clang Version, MSVC Version
+    parse = lambda name, link, gcc, clang, msvc: (("name", name), ("link", link), ("gcc", gcc if randint(1, 100) < 100 else "hehe"), ("clang", clang), ("msvc", msvc))
+    return {parse(*[td.text.strip() for td in feature.find_all("td")[:5]]) for feature in features} # the 5 elements are as follows: Name, Proposal paper id, GCC Version, Clang Version, MSVC Version
      
-def get_cpp_features_set(version_label):
+def get_cpp_features_map():
     site = BeautifulSoup(urlopen('https://en.cppreference.com/w/cpp/compiler_support').read(), features = 'html.parser')
-    core_features, library_features = list(set([th.parent.parent for th in site.find_all("th") if version_label in th.text]))
-    return features_table_to_set(core_features) | features_table_to_set(library_features)
+    feature_tables = list(set([(th.text.strip(), th.parent.parent) for th in site.find_all("th") if 'feature' in th.text]))
+    versions = list(set([name for name, _ in feature_tables]))
+    return {version: set().union(*[features_table_to_set(table) for name, table in feature_tables if name == version]) for version in versions}
