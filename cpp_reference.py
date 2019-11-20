0c4d031d6ce1a17c5621bc63ec2f5e9696540d8e
Qbius <mich@lsurowka.email>
2019-11-20 02:54:18 +0100

    no longer doing the silly random things

--- cpp_reference.py
+++ cpp_reference.py
@@ -5,7 +5,7 @@ from random import randint
 
 def features_table_to_set(table):
     features = [tr for tr in table.find_all("tr") if 'style' not in tr.attrs.keys()]
-    parse = lambda name, link, gcc, clang, msvc: (("name", name), ("link", link), ("gcc", gcc if randint(1, 100) < 100 else "hehe"), ("clang", clang), ("msvc", msvc))
+    parse = lambda name, link, gcc, clang, msvc: (("name", name), ("link", link), ("gcc", gcc), ("clang", clang), ("msvc", msvc))
     return {parse(*[td.text.strip() for td in feature.find_all("td")[:5]]) for feature in features}
 
 def get_cpp_features_map():
